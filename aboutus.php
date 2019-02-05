58d29054c4a05bde50d4a1813fff0eca5534a061
Vilboy <vilboyhacker@gmail.com>
2019-02-05 13:03:01 +0530

    added about,contact pages

---
+++ aboutus.php
@@ -0,0 +1,12 @@
+<?php include "header.html" ?>
+
+<div class="container">
+  <h2>About Us .....</h2>
+ 
+    <div class="panel panel-info">
+      <div class="panel-heading">Sorry</div>
+      <div class="panel-body">This page  is under constraction..</div>
+    </div>
+
+
+<?php include "footer.html" ?>
---
+++ contactus.php
@@ -0,0 +1,10 @@
+<?php include "header.html" ?>
+<div class="container">
+  <h2>Contact Us .....</h2>
+ 
+    <div class="panel panel-info">
+      <div class="panel-heading">Sorry</div>
+      <div class="panel-body">This page  is under constraction..</div>
+    </div>
+
+<?php include "footer.html" ?>
